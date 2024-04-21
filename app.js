5e84ae621507b5ce123ba8797c3cf0e7ec0f1ebc
kelvin <kelvin80121@gmail.com>
2024-04-21 16:04:10 +0800

    Refactor route paths in app.js to use '/courses' prefix

--- app.js
+++ app.js
@@ -29,8 +29,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use('/users', usersRouter);
 app.use('/courses', courseRouter);
+app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
