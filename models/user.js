184d7d5f41ebcf3478447832654044af7322ffff
kelvin <kelvin80121@gmail.com>
2024-06-09 23:13:15 +0800

    chore: Update user model with additional fields
    This commit updates the user model in the `user.js` file to include additional fields such as `photo`, `intro`, `facebook`, `instagram`, and `discord`. These fields allow users to provide more information about themselves and enhance their profiles. The commit also includes validation for the URL format of the `photo`, `facebook`, `instagram`, and `discord` fields to ensure data integrity.

--- models/user.js
+++ models/user.js
@@ -9,12 +9,12 @@ const userSchema = new Schema({
             validator: function(value) {
                 return validator.isLength(value, { min: 2 });
             },
-            message: 'name 至少需要 2 個字元以上'
+            message: '名稱至少需要 2 個字元以上'
         }
     },
     email: {
         type: String,
-        required: [true, 'email 未填寫'],
+        required: [true, 'Email 未填寫'],
         unique: true,
         index: true,
         validate: {
@@ -26,21 +26,24 @@ const userSchema = new Schema({
     },
     password: {
         type: String,
-        required: [true, 'password 未填寫'],
+        required: [true, '密碼未填寫'],
         minlength: 8,
         select: false
     },
     phone: {
-      type: String,
-      validate: {
-          validator: function(value) {
-              return validator.isMobilePhone(value, 'any', { strictMode: false });
-          },
-          message: '手機號碼格式不正確'
-      }
+        type: String,
+        validate: {
+            validator: function(value) {
+                return validator.isMobilePhone(value, 'any', { strictMode: false });
+            },
+            message: '手機號碼格式不正確'
+        }
     },
-    birthday: Date,
+    birthday: {
+        type: Date
+    },
     address: {
+        type: String,
         detail: String
     },
     gender: {
@@ -50,27 +53,70 @@ const userSchema = new Schema({
         required: [true, '性別未填寫']
     },
     courses: [{
-      type: Schema.Types.ObjectId,
-      ref: 'Courses'
+        type: Schema.Types.ObjectId,
+        ref: 'Courses'
     }],
     role: {
-      type: String,
-      enum: ['admin', 'user', 'superuser'],
-      default: 'user'
+        type: String,
+        enum: ['admin', 'user', 'superuser'],
+        default: 'user'
     },
     verificationToken: {
-      type: String,
-      default: '',
-      select: false
+        type: String,
+        default: '',
+        select: false
+    },
+    photo: {
+        type: String,
+        validate: {
+            validator: function(value) {
+                return validator.isURL(value);
+            },
+            message: '大頭照 URL 格式不正確'
+        }
+    },
+    intro: {
+        type: String,
+        maxlength: 500,
+        default: ''
+    },
+    facebook: {
+        type: String,
+        validate: {
+            validator: function(value) {
+                return validator.isURL(value);
+            },
+            message: 'Facebook URL 格式不正確'
+        }
+    },
+    instagram: {
+        type: String,
+        validate: {
+            validator: function(value) {
+                return validator.isURL(value);
+            },
+            message: 'Instagram URL 格式不正確'
+        }
+    },
+    discord: {
+        type: String,
+        validate: {
+            validator: function(value) {
+                return validator.isURL(value);
+            },
+            message: 'Discord URL 格式不正確'
+        }
     }
 }, {
     versionKey: false,
     timestamps: true
 });
 
+// 密碼加密和電郵格式化的 pre-save 鉤子
 // userSchema.pre('save', async function(next) {
-//     if (!this.isModified('password')) return next();
-//     this.password = await bcrypt.hash(this.password, 12);
+//     if (this.isModified('password')) {
+//         this.password = await bcrypt.hash(this.password, 12);
+//     }
 //     this.email = this.email.toLowerCase().trim();
 //     next();
 // });
