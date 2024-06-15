876ef175e9aebac1126d6c19455f4218eeb50725
kelvinLin9 <kelvin80121@hotmail.com>
2024-06-15 14:19:06 +0800

    Refactor getUser function to populate courses property

--- controllers/user.js
+++ controllers/user.js
@@ -106,20 +106,26 @@ const check = async (req, res) => {
 
 const getUser = async (req, res, next) => {
     try {
-      const user = await UsersModel.findById(req.user.userId)
-                                   .populate('courses');
-      if (!user) {
-          return res.status(404).send({
-              status: false,
-              message: '用戶未找到'
-          });
-      }
+        const user = await UsersModel.findById(req.user.userId)
+                                     .populate({
+                                         path: 'courses', // 確保這個 path 正確對應到 User 模型的 courses 屬性
+                                         select: 'name intro category place mode minutes price img imgList instructor' // 這裡可以根據需要選擇填充的字段
+                                     })
+                                     .exec(); // 執行查詢
 
-      res.send({
-          status: true,
-          result: user
-      });
+        if (!user) {
+            return res.status(404).send({
+                status: false,
+                message: '用戶未找到'
+            });
+        }
+
+        res.send({
+            status: true,
+            result: user
+        });
     } catch (error) {
+        console.error('Error fetching user:', error);
         next(error);
     }
 };
