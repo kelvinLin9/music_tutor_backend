6053262b4e1da82c4c33f474efbddc804dd499c4
kelvin <kelvin80121@gmail.com>
2024-06-16 21:29:05 +0800

    feat: Populate couponUsed field in getOrders and getOrder controllers
    The code changes modify the `getOrders` and `getOrder` controllers in the `order.js` file. The controllers now populate the `couponUsed` field using the `Coupon` model. This allows the `orders` and `order` objects to include the coupon information, such as name, code, and percentage.
    
    This commit message follows the convention of starting with a type (`feat` for a new feature) followed by a concise and descriptive message of the changes made.

--- controllers/order.js
+++ controllers/order.js
@@ -24,10 +24,15 @@ const getOrders = async (req, res) => {
       const orders = await OrderModel.find()
                                 .populate({
                                     path: 'items.product',
-                                    model: 'Course', // 確保你的課程模型名稱與此相符
-                                    select: 'name description price' // 只選擇需要的課程字段
+                                    model: 'Course',
+                                    select: 'name description price'
                                 })
-                                .populate('user', 'name email'); // 同時填充用戶資訊
+                                .populate('user', 'name email')
+                                .populate({
+                                  path: 'couponUsed',
+                                  model: 'Coupon',
+                                  select: 'name code percentage'
+                                });
 
       res.status(200).json({ success: true, data: orders });
   } catch (error) {
@@ -42,10 +47,15 @@ const getOrder = async (req, res) => {
       const order = await OrderModel.findById(req.params.id)
                                .populate({
                                    path: 'items.product',
-                                   model: 'Course', // 確保你的課程模型名稱與此相符
-                                   select: 'name description price' // 只選擇需要的課程字段
+                                   model: 'Course',
+                                   select: 'name description price'
                                })
-                               .populate('user', 'name email'); // 同時填充用戶資訊，選擇性填充字段如姓名和郵箱
+                               .populate('user', 'name email')
+                               .populate({
+                                path: 'couponUsed',
+                                model: 'Coupon',
+                                select: 'name code percentage'
+                              });
 
       if (!order) {
           return res.status(404).json({ success: false, message: 'Order not found' });
