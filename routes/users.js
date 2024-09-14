7a4042adafd90758b50562faca23f596af390d74
kelvin <kelvin80121@gmail.com>
2024-09-14 20:03:21 +0800

    refactor: Update callbackURL in users.js to use environment-specific URL

--- routes/users.js
+++ routes/users.js
@@ -21,7 +21,7 @@ import { handleErrorAsync } from '../statusHandle/handleErrorAsync.js';
 const callbackURL = process.env.NODE_ENV === 'production'
   // ? 'https://kelvinlin9.github.io/music_tutor_dashboard/google/callback'
   ? 'http://localhost:3010/music_tutor_dashboard/google/callback'
-  : 'http://localhost:3000/users/google/callback';
+  : 'http://localhost:3000/users/googleClient/callback';
 
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
