171ceeed605fa8705dc2c0e12fcac1ce175261d0
kelvin <kelvin80121@gmail.com>
2024-09-18 18:14:57 +0800

    refactor: Update googleLogin function in auth.js to include token generation and redirection

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,13 +4,13 @@ import { handleErrorAsync } from '../statusHandle/handleErrorAsync.js';
 
 const googleLogin = handleErrorAsync(async (req, res, next) => {
   console.log("googleLogin")
-  // console.log('req', req.user);
+  console.log('req', req.user);
   const token = generateToken({ userId: req.user._id })
-  res.send({
-    status: true,
-    user: req.user,
-    token: token
-  });
+  // res.send({
+  //   status: true,
+  //   user: req.user,
+  //   token: token
+  // });
   res.redirect(`${state}?token=${token}`);
 });
 
