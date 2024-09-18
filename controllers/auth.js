f40ad8e95c94956582363c172539e83cd9001b16
kelvin <kelvin80121@gmail.com>
2024-09-18 18:29:30 +0800

    refactor: Update generateToken function to use user._id instead of user.userId
    - Remove the unnecessary console.log statement in the googleLogin function
    - Modify the generateToken function in utils/index.js to use user._id instead of user.userId
    - Add the role property to the payload in generateToken function

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,11 +1,11 @@
 import UsersModel from '../models/user.js'
-import { generateToken, verifyToken } from '../utils/index.js';
+import { generateToken } from '../utils/index.js';
 import { handleErrorAsync } from '../statusHandle/handleErrorAsync.js';
 
 const googleLogin = handleErrorAsync(async (req, res, next) => {
   console.log("googleLogin")
   console.log('req', req.user);
-  const token = generateToken({ userId: req.user._id })
+  const token = generateToken({ userId: req.user._id, role: req.user.role })
   // res.send({
   //   status: true,
   //   user: req.user,
--- utils/index.js
+++ utils/index.js
@@ -5,10 +5,10 @@ export const generateToken = (user) => {
   if (!process.env.JWT_SECRET || !process.env.JWT_EXPIRES_DAY) {
       throw new Error("Required JWT environment variables are not set.");
   }
-  // console.log(user)
+  console.log('user', user)
   // 生成 payload，包括用戶 ID 和角色
   const payload = {      
-      userId: user._id,
+      userId: user.userId,
       role: user.role,
   };
   console.log("Payload:", payload);
